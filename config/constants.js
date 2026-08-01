f4da6f4bce5e7257f41335b23af01abbf68961cf
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-08-01 11:58:44 +0800

    the config constants

---
+++ config/constants.js
@@ -0,0 +1,26 @@
+export const CANVAS = {
+  width: 500,
+  height: 500,
+};
+
+export const FPS = 60;
+
+export const CONTROLS = {
+  ArrowUp: 'moveUp',
+  ArrowDown: 'moveDown',
+  ArrowLeft: 'moveLeft',
+  ArrowRight: 'moveRight',
+  ' ': 'shoot',
+};
+
+export const SCENES = {
+  MENU: 'menu',
+  GAME: 'game',
+  GAME_OVER: 'gameOver',
+};
+
+export const PALETTE = {
+  background: '#111',
+  canvas: '#222',
+  accent: 'aqua',
+};
